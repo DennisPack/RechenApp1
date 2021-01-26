4e4f4b2956d1584ee2b1dc47c5eca90e0c8c4150
dpack <dennis.pack@smail.th-koeln.de>
2021-01-26 14:58:21 +0100

    LiveData und Kommentare hinzugefügt

--- app/src/main/java/com/example/rechenapp1/view/RechnenActivity.kt
+++ app/src/main/java/com/example/rechenapp1/view/RechnenActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.rechenapp1.R
 import com.example.rechenapp1.databinding.ActivityMainBinding
@@ -11,9 +12,9 @@ import com.example.rechenapp1.viewModel.RechnenViewModel
 
 class RechnenActivity : AppCompatActivity() {
 
-    //ViewModel
+    // ViewModel
     private lateinit var meinViewModel : RechnenViewModel
-    //DataBinding
+    // DataBinding
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,25 +24,42 @@ class RechnenActivity : AppCompatActivity() {
         // DataBinding - Statt "setContentView(R.layout.activity_main)"
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        // ViewModel von Hilfsklasse zuweisen lassen
+        // ViewModel von Java-Hilfsklasse zuweisen lassen - Hilfsklasse verlangt eine Activity (this) und ein ViewModel (RechnenViewModel)
         meinViewModel = ViewModelProvider(this).get(RechnenViewModel::class.java)
 
-        // Buttons
+        // Buttons mit DataBinding statt FindViewById
         binding.buttonRechnen.setOnClickListener {
             meinViewModel.rechnen(binding.EingabeZahl.text.toString().toInt())
-            // UI aktualisieren
-            binding.feldAktuellesErgebnis.text = meinViewModel.aktuellesErgebnis.toString()
-            binding.feldBisherigesErgebnis.text = meinViewModel.bisherherigesErgebnis.toString()
         }
         binding.buttonReset.setOnClickListener {
             meinViewModel.zuruecksetzen()
-            // UI aktualisieren
-            binding.feldAktuellesErgebnis.text = meinViewModel.aktuellesErgebnis.toString()
-            binding.feldBisherigesErgebnis.text = meinViewModel.bisherherigesErgebnis.toString()
         }
+
+        //   ALTE Methode aus Präsentation um die Texte in der Ansicht zu aktualisieren:
+        //
+        //   binding.buttonReset.setOnClickListener {
+        //     meinViewModel.zuruecksetzen()
+        //
+        //    // UI aktualisieren
+        //    //binding.feldAktuellesErgebnis.text = meinViewModel.aktuellesErgebnis.toString()
+        //    //binding.feldBisherigesErgebnis.text = meinViewModel.bisherherigesErgebnis.toString()
+        //    }
+        //
+        //    Hier haben wir die View-Felder immer manuell aktualisiert, wenn wir den Button gedrückt haben.
+        //    Dies hatte jedoch den Nachteil, dass nach einer Bildschirmdrehung erst die Default-Werte (0) angezeigt wurden, bis der Button dann erneut gedrückt wurde.
+
+        // NEUE Methode zum aktualisieren mit Observer und LiveData
+        // Mit dem ersten Teil (bis zum {}) erstellen wir einen Observer, der unsere LiveData-Objekte überwacht.
+        // Dieser muss einer Activity (einem Lifecycle-Owner) zugeordnet werden (this).
+        // in der {} sagen wir dann, was passieren soll, wenn sich unser LiveData-Objekt ändert -> In unserem Fall die Aktualisierung des View-Feldes.
+        // Dadurch werden die aktuellen Ergebnisse nun auch DIREKT nach der Drehung des Bildschirms angezeigt.
+        meinViewModel.bisherigesLiveDataErgebnis.observe(this, Observer { binding.feldBisherigesErgebnis.text = it.toString() })
+        meinViewModel.aktuellesLiveDataErgebnis.observe(this, Observer { binding.feldAktuellesErgebnis.text = it.toString() })
     }
 
-    // Activity Status
+
+    // Activity Zustände -> Um diese bei der Ausführung zu sehen, unter Logcat nach dem Tag "LOGS" filtern
+    // Hat keine Relevanz für die Rechenapp
     override fun onStart() {
         super.onStart()
         Log.i("LOGS", "--> onStart aufgerufen")
--- app/src/main/java/com/example/rechenapp1/viewModel/RechnenViewModel.kt
+++ app/src/main/java/com/example/rechenapp1/viewModel/RechnenViewModel.kt
@@ -1,25 +1,47 @@
 package com.example.rechenapp1.viewModel
 
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.rechenapp1.model.Rechnung
 
 class RechnenViewModel : ViewModel() {
+
     private val rechnung = Rechnung()
-    var aktuellesErgebnis = rechnung.gibErgebnis()
-    var bisherherigesErgebnis = aktuellesErgebnis
+
+    // Kapselung der LiveData-Objekte:
+    // Wir wollen nicht, dass von überall auf unsere LiveData-Objekte zugegriffen werden kann. Die Observer müssen aber auf diese zugreifen, damit sie diese beobachten können.
+    // Deshalb Kapseln wir die Objekte und stellen eine "öffentliche" Variante der Objekte zur Verfügung:
+
+    // ViewModel interne private Variablen - Diese werden mit einem Unterstrich gekennzeichnet - veränderbare MutableLiveData-Objekte.
+    private var _aktuellesLiveDataErgebnis : MutableLiveData<Int> = MutableLiveData(rechnung.gibErgebnis())
+    private var _bisherigesLiveDataErgebnis : MutableLiveData<Int> = MutableLiveData()
+
+    // Öffentliche Variablen nach außen hin (View). Dies sind nicht veränderbare LiveData-Objekte und dienen zum aktualisieren der View.
+    // Hierauf werden in der Activity die Observer gesetzt.
+    val aktuellesLiveDataErgebnis : LiveData<Int>
+        get() = _aktuellesLiveDataErgebnis
+    val bisherigesLiveDataErgebnis : LiveData<Int>
+        get() = _bisherigesLiveDataErgebnis
 
 
     fun rechnen(eingabe : Int){
-        bisherherigesErgebnis = rechnung.gibErgebnis()
-        aktuellesErgebnis = rechnung.rechnen(eingabe)
+        // Alt
+        //bisherherigesErgebnis = rechnung.gibErgebnis()
+        //aktuellesErgebnis = rechnung.rechnen(eingabe)
 
+        // NEU mit LiveData - ACHTUNG!: MutableLiveData-Objekte können nur geändert werden, indem man den "value" des Objektes ändert. NICHT das Objekt selbst!
+        // (siehe auch Kotlin: Backing-Field)
+        //_bisherherigesErgebnisLiveData = rechnung.gibErgebnis()  <--- Falsch! Geht nicht!
+
+        _bisherigesLiveDataErgebnis.value = rechnung.gibErgebnis()
+        _aktuellesLiveDataErgebnis.value = rechnung.rechnen(eingabe)
     }
 
     fun zuruecksetzen(){
         rechnung.zurueckSetzen()
-        aktuellesErgebnis = rechnung.gibErgebnis()
-        bisherherigesErgebnis = aktuellesErgebnis
+        _aktuellesLiveDataErgebnis.value = rechnung.gibErgebnis()
+        _bisherigesLiveDataErgebnis.value = _aktuellesLiveDataErgebnis.value
     }
-
 }
